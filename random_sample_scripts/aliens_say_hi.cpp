d18e792f0b71594e8f76323408fd49c2e8eb278e
Sunit <sunit.singh@tonboimaging.com>
2017-10-09 16:11:08 +0530

    Aliens Respond Spelling Correction

--- random_sample_scripts/aliens_say_hi.cpp
+++ random_sample_scripts/aliens_say_hi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	printf("Hello Eartlings\n");
+	printf("Hello Earthlings\n");
 	printf("You are pretty darn vulnerable, you sack of douchebags!\n");
 	return 0;
 }
